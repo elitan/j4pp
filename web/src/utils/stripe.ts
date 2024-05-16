ad412548f3230319bd1665739f18559c5d5e9c34
Johan Eliasson <johan@eliasson.me>
2024-05-16 22:45:33 +0200

    x

--- web/src/utils/stripe.ts
+++ web/src/utils/stripe.ts
@@ -4,5 +4,5 @@ import Stripe from "stripe";
 import { env } from "@/utils/env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-04-10",
 });
