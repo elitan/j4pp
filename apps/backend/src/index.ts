13483709d3bd57f525c0bf2957f01c630df5a1c1
Johan Eliasson <johan@eliasson.me>
2025-02-02 20:05:16 +0100

    wip

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,27 +1,46 @@
-import { Hono } from "hono";
-import { serve } from "@hono/node-server";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "./trpc";
-import { serveStatic } from "@hono/node-server/serve-static";
-import { logger } from "hono/logger";
+import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { logger } from 'hono/logger'
+import { appRouter } from './trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { spawnSync } from 'bun'
+import { swaggerUI } from '@hono/swagger-ui'
 
-const app = new Hono();
+const app = new Hono()
 
-app.use("*", logger());
+app.use('*', logger())
 
-app.use("/api/trpc/*", async (c) => {
+app.use('/api/trpc/*', async (c) => {
   const res = await fetchRequestHandler({
-    endpoint: "/api/trpc",
+    endpoint: '/api/trpc',
     req: c.req.raw,
     router: appRouter,
     createContext: () => ({}),
-  });
-  return res;
-});
+  })
+  return res
+})
 
-app.use("/static/*", serveStatic({ root: "./static" }));
-app.get("*", serveStatic({ path: "./dist/index.html" }));
+app.use('/api/test', async (c) => {
+  // for spawnSync, it is a property on the return value
+  const { resourceUsage } = spawnSync([
+    'bun',
+    '-e',
+    "console.log('Hello world!')",
+  ])
 
-const port = 3001;
-console.log(`Server running at http://localhost:${port}`);
-serve({ fetch: app.fetch, port });
+  console.log(resourceUsage)
+
+  return c.json({ message: 'Hello World' })
+})
+
+app.use('/static/*', serveStatic({ root: './static' }))
+app.get('*', serveStatic({ path: './dist/index.html' }))
+
+app.get('/ui', swaggerUI({ url: '/doc' }))
+const port = 3001
+console.log(`Server running at http://localhost:${port}`)
+
+export default {
+  port: 3001,
+  fetch: app.fetch,
+}
